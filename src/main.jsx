a3b508ac936eb9054c298fa26eccfdb89c331eba
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-20 12:38:50 +0600

    Create routes folder and move Home component to routes/

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Home from './Components/Home';
+import Home from './Components/routes/Home';
 import './index.css';
 import ErrorPage from './error-page';
 

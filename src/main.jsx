d53f18908c08a931329da4c6033455dee1aa6c22
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-20 12:26:07 +0600

    Added Home Components.

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App';
+
+import Home from './Components/Home';
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App></App>,
-  },
-  {
-    path: '/about',
-    element: <h1>This is about page</h1>,
+    element: <Home></Home>,
   },
 ]);
 
